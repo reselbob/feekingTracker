03dd94313cfb1dede1fa98fb15e95ef188d7016a
Bob Reselman <reselbob@gmail.com>
2018-10-15 09:22:44 -0700

    debugging bat file

--- winbuild.bat
+++ winbuild.bat
@@ -11,4 +11,13 @@ type temp.txt
 for /f "delims=" %%x in (temp.txt) do set myservice=%%x
 echo The service return is: %myservice%
 
+set n=0
+for %%a in ( %myservice% ) do (
+   set vector[!n!]=%%a
+   set /A n+=1
+)
+
+set serviceurl=!vector[3]!
+echo The service url is: %serviceurl%
+
 del temp.txt
