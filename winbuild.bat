e54ea91166311de7b4727eaded1923b8f51a787f
Bob Reselman <reselbob@gmail.com>
2018-10-15 00:31:00 -0700

    adding winbuild.bat

---
+++ winbuild.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal EnableDelayedExpansion
+echo %1
+
+kubectl get service | findstr %1 > temp.txt
+
+set /p myservice=<temp.txt
+
+del temp.txt
+echo The service return is: %myservice%
+
+set n=0
+for %%a in ( %myservice% ) do (
+   set vector[!n!]=%%a
+   set /A n+=1
+)
+set serviceurl=!vector[3]!
+echo The service url is: %serviceurl%
