2ead73225fafec753f725713d4e250bd1370a11a
Bob Reselman <reselbob@gmail.com>
2018-10-15 11:23:46 -0700

    debugging bat file

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 
 setlocal EnableExtensions EnableDelayedExpansion
 echo %1
-ping -n 45 127.0.0.1 >nul
+ping -n 10 127.0.0.1 >nul
 
 kubectl get service | findstr %1 > temp.txt
 
@@ -12,10 +12,8 @@ echo The service return is: %myservice%
 
 set n=0
 for %%a in ( %myservice% ) do (
-set vector[!n!]=%%a
-set /A n+=1
+  echo %%a
 )
-set serviceurl=!vector[3]!
-echo The service url is: %serviceurl%
+
 
 del temp.txt
