206bc4b4be81da37d84b1e2915166962e3ed69c4
Bob Reselman <reselbob@gmail.com>
2018-10-14 18:02:08 -0700

    adding build instructions in wincloud/build.bat

--- wincloud/build.bat
+++ wincloud/build.bat
@@ -1,1 +1,15 @@
-echo "Access is $GC_ACCESS"
+@echo off
+setlocal EnableDelayedExpansion
+echo %1
+kubectl get service | findstr %1 > temp.txt
+set /P myservice=<temp.txt
+echo The service return is: %myservice%
+
+set n=0
+for %%a in ( %myservice% ) do (
+   set vector[!n!]=%%a
+   set /A n+=1
+)
+
+set serviceurl=!vector[3]!
+echo The service url is: %serviceurl%
